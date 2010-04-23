d6f0f9ff26fdd723189f23c9484045bb46aae271
Sven Ginka <sven.ginka@gmail.com>
2010-04-24 00:52:21 +0200

    added IPodMp (directly no submodule)

--- lib/ClientHandler.php
+++ lib/ClientHandler.php
@@ -90,8 +90,10 @@
 /* temporary disabled
 		$this->addClient( 'phpMp2', 'phpMp2', 'v0.11.0 - client by mpd team');
 		$this->addClient( 'phpMp3', 'phpMp3', 'v0.2 - client by <a href="http://sourceforge.net/projects/phpmp3/">angry_elf</a>');
-		$this->addClient( 'IPodMp', 'IPodMp', 'client by Hendrik Stoetter 03/2008 <a href="http://www.itrium.de/pages/home/mpd_ipod_touch_musikserver_remote_wlan.php?font_size=100">link</a>');
+		
 */
+		$this->addClient( 'IPodMp', 'ipodmp', 'client by Hendrik Stoetter 03/2008 <a href="http://www.itrium.de/pages/home/mpd_ipod_touch_musikserver_remote_wlan.php?font_size=100">link</a>');
+		
 		//only for testing
 		//$this->addClient( 'mpd.class.test', 'phpMpClassTest', 'test client by tswaehn');		 		
  	}
---
+++ web_clients/ipodmp/config.php
@@ -0,0 +1,20 @@
+<?php
+// NEED TO SET THESE!
+$config_host = "192.168.2.35";
+$config_port = 6600;
+
+// OPTIONAL
+$title = "iPodMp";
+$default_song_display_conf = "artist - title";
+//$default_sort = "file,Artist,Album,Track,Title";
+$default_sort = "file";
+$filenames_only = "yes";
+ 
+
+// VOLUME OPTIONS
+$volume_incr = "10";
+
+ 
+
+
+?>
