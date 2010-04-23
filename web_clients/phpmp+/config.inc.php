7068564d5860bb9204d67076250dd93d20d8a839
Sven Ginka <sven.ginka@gmail.com>
2010-04-24 00:48:42 +0200

    added phpmp+ (directly no submodule)

---
+++ web_clients/phpmp+/config.inc.php
@@ -0,0 +1,11 @@
+<?php
+$cfg["host"] = "192.168.2.35";
+$cfg["port"] = 6600;
+$cfg["style"] = "default";
+$cfg["title"] = "Music Player";
+$cfg["refresh"] = 30;
+$cfg["playlist_length"] = 20;
+$cfg["lang"] = "fr";
+$cfg["client_password"] = "abcd";
+$cfg["song_display"] = "(artist) title??filename";
+?>
